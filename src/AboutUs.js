42e47c37b195b4327048ea410a9d0b15b8f1d7b0
wojtek <navjeetkajal.2594@gmail.com>
2022-06-13 17:55:28 +0530

    some improvement

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import noUs from "./images/no-us.png"
 
 const AboutUs = () => {
-  return (
-    <div style={{display : "flex", justifyContent : "center", height : "100vh", alignItems : "center", color : "wheat", backgroundColor: "#333333"}}>
-    <img alt="" src={noUs} />
-</div>
-  )
+    return (
+        <div style={{ display: "flex", justifyContent: "center", height: "100vh", alignItems: "center", color: "wheat", backgroundColor: "#333333" }}>
+            <img alt="" src={noUs} />
+            <Link type='button' to="/">
+                <button className='linkBtn' style={{margin : "0 0 0 50px"}}>
+                    Go back to your miserable life
+                </button>
+            </Link>
+        </div>
+    )
 }
 
 export default AboutUs
--- src/Header.js
+++ src/Header.js
@@ -4,28 +4,27 @@ import { Link } from "react-router-dom";
 
 
 function Header() {
-    return ( 
-        <div className = "header container fluid">
+    return (
+        <div className="header container fluid">
             <div className="headerLeft">
                 <img alt="skull" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Black_skull.svg/1024px-Black_skull.svg.png" />
             </div>
             <div className="headerMiddle">
-               Skull Meme Generator
+                Skull Meme Generator
             </div>
             <div className="headerRight">
-               
-                {/* <button className="linkBtn"><span>About Us</span></button> */}
                 <Link
                     type="button"
-                    className="linkBtn"
                     to="/aboutUs"
-                  >
-                    <span>About Us</span>
-                  </Link>
+                >
+                    <button className="linkBtn">
+                        <span>About Us</span>
+                    </button>
+                </Link>
             </div>
         </div>
 
-        )
+    )
 }
 
 export default Header
