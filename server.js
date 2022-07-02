80822dd059466c4d95141363019753e8f875efce
wojtek <navjeetkajal.2594@gmail.com>
2022-07-02 23:41:33 +0530

    equipped with nodeJs, mongo Db, express

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require('mongoose');
+const path = require('path')
+
+const app = express();
+app.use(express.json());
+  
+
+// Connection with Mongodb.
+mongoose.connect(
+    process.env.CONNECTION_URL,
+      {
+        useNewUrlParser: true,
+        // useCreateIndex: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false,
+      },
+      (err) => {
+        if (err) throw err;
+        console.log("connected to mongodb");
+      },
+    );
+    
+  
+const port = process.env.PORT;
+  
+app.listen(port,() => {
+	console.log(`listening port localhost : ${port}`);
+});
