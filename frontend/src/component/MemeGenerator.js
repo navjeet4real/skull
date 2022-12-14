a6ea8c5fa7c7cae8e9b684fde0e9e9a7762e707a
“navjeet4real” <navjeetkajal.2594@gmail.com>
2022-12-14 12:16:50 +0530

    converted class cmpnt to func

--- frontend/src/component/MemeGenerator.js
+++ frontend/src/component/MemeGenerator.js
@@ -1,67 +1,63 @@
-import React, {Component} from "react"
+import React, { Fragment, useState,useEffect } from "react";
+const  initialState = { topText : "", bottomText: "" } 
 
-class MemeGenerator extends Component{
-    constructor(){
-        super()
-        this.state = {
-            topText: "",
-            bottomText: "",
-            randomImg: "https://i.imgflip.com/46e43q.png",
-            allMemeImgs: []
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-    componentDidMount(){
+const MemeGeneratorMain = () => {
+  const [allMeme, setAllMeme] = useState([]);
+  const [text, setText] = useState(initialState);
+  const [randomImg, setRandomImg] = useState("https://i.imgflip.com/46e43q.png");
+
+  const {topText, bottomText} = text
+    useEffect(() => {
+     getAllMeme()
+    }, [])
+    function getAllMeme () {
         fetch("https://api.imgflip.com/get_memes")
         .then(response => response.json())
         .then(response => {
-            const {memes} = response.data
-            this.setState({ allMemeImgs: memes})
+            setAllMeme(response.data)
         })
     }
-    handleChange(event){
-        const {name, value} = event.target
-        this.setState({
-            [name]: value
-        })
-    }
-    handleSubmit(event){
-        event.preventDefault()
-        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
-        console.log(randNum)
-        const randMemeImg = this.state.allMemeImgs[randNum].url
-        this.setState({ randomImg: randMemeImg})
-    }
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setText({
+        [name] : value
+    })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const randNum = Math.floor(Math.random() * allMeme.length)
+    console.log(randNum)
+    const randMemeImg = allMeme[randNum].url
+    setRandomImg(randMemeImg)
+  }
+  return (
+    <Fragment>
+      <div>
+        <form className="form col sm={12}" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Top text"
+            name="topText"
+            value={topText}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Bottom text"
+            name="bottomText"
+            value={bottomText}
+            onChange={handleChange}
+          />
+          <button type="submit" className="button-54 col sm={6}">Change Meme</button>
+        </form>
+        <div className="meme">
+          <img src={randomImg} alt="" />
+          <h2 className="topText">{topText}</h2>
+          <h2 className="bottomText">{bottomText}</h2>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-    render(){
-        return(
-            <div>
-                <form className="form col sm={12}" onSubmit={this.handleSubmit}>
-                    <input 
-                        type="text"
-                        placeholder = "Top text"
-                        name = "topText"
-                        value = {this.state.topText}
-                        onChange = {this.handleChange}
-                    />
-                    <input 
-                        type="text"
-                        placeholder="Bottom text"
-                        name = "bottomText"
-                        value = {this.state.bottomText}
-                        onChange = {this.handleChange}
-                    />
-                    <button className="button-54 col sm={6}">Change Meme</button>
-                </form>
-                <div className="meme">
-                    <img src={this.state.randomImg} alt=""/>
-                    <h2 className="topText">{this.state.topText}</h2>
-                    <h2 className="bottomText">{this.state.bottomText}</h2>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default MemeGenerator
+export default MemeGeneratorMain;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require('mongoose');
-const path = require('path')
 
 const app = express();
 app.use(express.json());
