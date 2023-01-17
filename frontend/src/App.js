d8dd298485ffbcc9f2803ec115ff18d01e8300fd
navjeet4real <navjeetkajal.2594@gmail.com>
2023-01-17 18:29:53 +0530

    sign in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./component/Home";
 import "./App.css";
 import AboutUs from "./component/AboutUs";
-import { useEffect } from "react";
 import Login from "./component/Login";
 import MemeDashboard from "./component/MemeDashboard";
 
 function App() {
-  
   return (
-    <BrowserRouter>
-      <div>
-        <Routes>
-        <Route exact path="/" element={<Login />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/aboutUs" element={<AboutUs />}></Route>
-          <Route exact path="/dashboard" element={<MemeDashboard />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/aboutUs" element={<AboutUs />}></Route>
+            <Route exact path="/dashboard" element={<MemeDashboard />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,44 +1,17 @@
-import React, { useEffect, useState } from "react";
-import jwt_decode from "jwt-decode";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postDataAPI } from "../utils/API";
+import { IconButton } from "@mui/material";
+import { GoogleLogo } from "phosphor-react";
+const initialState = {
+  email: "",
+  password: "",
+  err: "",
+  success: "",
+};
 const Login = () => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(initialState);
   let navigate = useNavigate();
-  function handleCredentialResponse(response) {
-    console.log("JWT token" + response.credential);
-    var userDetails = jwt_decode(response.credential);
-    console.log(userDetails);
-    setUser(userDetails);
-    document.getElementById("signIN").hidden = true;
-    createUser();
-    // navigate("/home");
-
-  }
-  // function onSignIn(googleUser) {
-  //   var id_token = googleUser.getAuthResponse().id_token;
-  //  console.log(id_token,"tokeeeeennnnnnn")
-  // }
-  //   if(user ){
-  //     createUser(user);
-
-  //   }
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id:
-        "183947174251-v0876c90evn4mddbt5m3uo1qq9movr7l.apps.googleusercontent.com",
-      callback: handleCredentialResponse,
-    });
-
-    google.accounts.id.renderButton(document.getElementById("signIN"), {
-      theme: "outline",
-      size: "large",
-      type: "icon"
-    });
-
-    google.accounts.id.prompt();
-  }, []);
 
   async function createUser() {
     const res = await postDataAPI("user/create-user", user);
@@ -53,14 +26,12 @@ const Login = () => {
   return (
     <>
       <div className="login-banner">
-        <div id="signIN" className="login-button"></div>
-      </div>
-      {/* {user && (
-        <div>
-          <img alt="profile-picture" src={user.picture} />
-          <h3>{user.name}</h3>
+        <div id="signIN" className="login-button">
+          <IconButton sx={{ backgroundColor: "white" }}>
+            <GoogleLogo size={32} color="#DF3E30" />
+          </IconButton>
         </div>
-      )} */}
+      </div>
     </>
   );
 };
