bdb02e7282fd1d5252c24ef554a67ef199d9f545
navjeet4real <navjeetkajal.2594@gmail.com>
2023-09-14 15:13:14 +0530

    changed render link

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export let BASE_URL
 if (window.location.origin.includes("http://localhost")) {
     BASE_URL = "http://localhost:8000"
-} else if (window.location.origin.includes("http://ip-address")) {
-    BASE_URL = "http://ip-address:8000"
+} else if (window.location.origin.includes("https://glowing")) {
+    BASE_URL = "https://skull-meme-generator.onrender.com"
 }
